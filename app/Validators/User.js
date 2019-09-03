386a1d44dfe88655d9090f3b76b958da10f056e2
humberto <humberto.obarbosa@gmail.com>
2019-09-02 21:27:41 -0300

    add validacao

---
+++ app/Validators/User.js
@@ -0,0 +1,17 @@
+'use strict'
+
+class User {
+  get validateAll () {
+    return true
+  }
+
+  get rules () {
+    return {
+      username: 'required|unique:users',
+      email: 'required|email|unique:users',
+      passowrd: 'required|confirmed'
+    }
+  }
+}
+
+module.exports = User
